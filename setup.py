df703d1a157daae0c4cf862af0c2f611b7d4c64b
crivero7 <cdrr.rivero@gmail.com>
2020-10-27 17:15:08 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+# setup.py
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="RackioAI",
+    version="0.0.5",
+    author="Carlos Rivero",
+    author_email="cdrr.rivero@gmail.com",
+    description="An useful decorators package for any projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/crivero7/easy-deco",
+    include_package_data=True,
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'tqdm==4.50.2',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
