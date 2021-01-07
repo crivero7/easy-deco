0a2737dca5d002a7ec253983ef40f48e22cb8397
Carlos Rivero <cdrr.rivero@gmail.com>
2021-01-07 12:12:19 -0400

    Adding generators to progress_bar

--- easy_deco/progress_bar.py
+++ easy_deco/progress_bar.py
@@ -27,7 +27,8 @@ def progress_bar(func, *args, **kwargs):
     ```
     """
     defaultOptions = {'desc': 'Reading files',
-                      'unit': 'Files'}
+                      'unit': 'Files',
+                      'gen': False}
 
     Options = {key: kwargs[key] if key in list(kwargs.keys()) else defaultOptions[key] for key in
                list(defaultOptions.keys())}
@@ -35,11 +36,15 @@ def progress_bar(func, *args, **kwargs):
     (self, iterableObject) = args[0]
 
     if not hasattr(iterableObject, '__iter__'):
-
         raise ValueError('You must provide an iterableObject in {}'.format(func.__name__))
 
     for i in tqdm(range(len(iterableObject)), desc=Options['desc'], unit=Options['unit']):
 
-        result = func(self, iterableObject[i])
+        if Options['gen']:
 
-    return result
+            yield func(self, iterableObject[i])
+
+        else:
+
+            result = func(self, iterableObject[i])
+            return result
