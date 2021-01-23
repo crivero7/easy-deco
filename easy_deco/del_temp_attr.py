4356736a9080d1deaa4b4056105064a418064be3
crivero7 <cdrr.rivero@gmail.com>
2021-01-23 11:45:28 -0400

    Adding del_temp_attr

---
+++ easy_deco/del_temp_attr.py
@@ -0,0 +1,51 @@
+import inspect
+
+class DelTempAttr(object):
+
+    def __init__(self):
+        """
+
+        """
+        pass
+        
+    def __call__(self, fn):
+        """
+        Decorator to delete all temporary attributes generated in each pipeline component
+
+        **Parameters**
+
+        * **:param f:** (Function) function to be decorated
+        """
+        def wrapper(*args, **kwargs):
+
+            result = f(*args, **kwargs)
+
+            attrs = inspect.getmembers(self, lambda attr:not(inspect.isroutine(attr)))
+
+            for attr, _ in attrs:
+
+                if attr.startswith('_') and attr.endswith('_'):
+                    
+                    if not(attr.startswith('__')) and not(attr.endswith('__')):
+
+                        delattr(self, attr)
+
+            self.data = result
+
+            return result
+
+        return wrapper
+
+def all_methods(decorator):
+    
+    def decorate(cls):
+        
+        for name, fn in inspect.getmembers(cls, inspect.ismethod):
+            
+            if not name.startswith('__'):
+                        
+                setattr(cls, name, decorator(fn))
+        
+        return cls
+    
+    return decorate
