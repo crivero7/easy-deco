90b569fac3c0b09f175339f33a41b969f6c48052
crivero7 <cdrr.rivero@gmail.com>
2021-01-23 16:53:32 -0400

    easy-deco 0.0.9

--- easy_deco/del_temp_attr.py
+++ easy_deco/del_temp_attr.py
@@ -1,56 +1,41 @@
-import inspect
+def del_temp_attr(fn):
+    """
+    Decorator to delete all temporary attributes generated in each pipeline component
+
+    **Parameters**
+
+    * **:param fn:** (Function) function to be decorated
+    """
+    def wrapper(*args, **kwargs):
+
+        result = fn(*args, **kwargs)
+        cls = args[0]
+
+        for instance in getattr(cls, "_instances"):
+
+            for attr in list(instance.__dict__.keys()):
+
+                if attr.startswith('_') and attr.endswith('_'):
+
+                    if not(attr.startswith('__') and attr.endswith('__')):
+
+                        delattr(instance, attr)
+
+        return result
+
+    return wrapper
 
 
-class Del_Temp_Attr(object):
+def set_to_methods(decorator):
 
-    def __init__(self, cls):
-        """
-
-        """
-        self._cls = cls
+    def decorate(cls):
         
-    def __call__(self, fn):
-        """
-        Decorator to delete all temporary attributes generated in each pipeline component
-
-        **Parameters**
-
-        * **:param fn:** (Function) function to be decorated
-        """
-        def wrapper(*args, **kwargs):
-
-            result = fn(*args, **kwargs)
-            cls = self._cls
-
-            for instance in getattr(cls, "_instances"):
-
-                for attr in list(instance.__dict__.keys()):
-
-                    if attr.startswith('_') and attr.endswith('_'):
-
-                        if not(attr.startswith('__') and attr.endswith('__')):
-                            
-                            delattr(instance, attr)
-
-            return result
-
-        return wrapper
-
-
-class DelTempAttr(type):
-
-    def __new__(meta, name, bases, class_dict):
-
-        klass = super().__new__(meta, name, bases, class_dict)
-
-        for key in dir(klass):
+        for attr in list(cls.__dict__.keys()):
             
-            value = getattr(klass, key)
+            if not attr.startswith('_'):
+                
+                setattr(cls, attr, decorator(getattr(cls, attr)))
+        
+        return cls
 
-            if not key.startswith('_'):
-
-                del_attr = Del_Temp_Attr(klass)
-                wrapped = del_attr(value)
-                setattr(klass, key, wrapped)
-
-        return klass
+    return decorate
